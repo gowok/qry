632c4353196f4b22920e7d7e5b9c1c60ce56fda6
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-05-22 21:59:52 +0800

    test(select): better test cases writing

--- select_test.go
+++ select_test.go
@@ -8,90 +8,29 @@ import (
 )
 
 func TestSelectToSQL(t *testing.T) {
-
-	t.Run("1", func(t *testing.T) {
-		expected := "SELECT 1"
-		result := qry.Select("1").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("distinct", func(t *testing.T) {
-		expected := "SELECT DISTINCT id FROM table"
-		result := qry.Select("id").Distinct().From("table").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("prepared", func(t *testing.T) {
-		expected := "SELECT * FROM table WHERE a = ?"
-		result := qry.Select().From("table").Where("a = ?").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("where", func(t *testing.T) {
-		expected := "SELECT * FROM table WHERE a = ? AND b = ?"
-		result := qry.Select().From("table").OrWhere("a = ?").AndWhere("b = ?").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("where 2", func(t *testing.T) {
-		expected := "SELECT * FROM table WHERE a = ? OR b = ?"
-		result := qry.Select().From("table").AndWhere("a = ?").OrWhere("b = ?").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("limit offset", func(t *testing.T) {
-		expected := "SELECT a, b FROM table LIMIT 1 OFFSET 1"
-		result := qry.Select("a", "b").From("table").Limit(1).Offset(1).SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("join", func(t *testing.T) {
-		expected := "SELECT * FROM table1 t1 JOIN table2 t2 ON t1.table2_id=t2.id LEFT JOIN table3 t3 ON t1.table3_id=t3.id RIGHT JOIN table4 t4 ON t1.table4_id=t4.id INNER JOIN table5 t5 ON t1.table5_id=t5.id"
-		result := qry.Select().From("table1 t1").
+	cases := map[string]string{
+		"SELECT 1":                                           qry.Select("1").SQL(),
+		"SELECT DISTINCT id FROM table":                      qry.Select("id").Distinct().From("table").SQL(),
+		"SELECT * FROM table WHERE a = ?":                    qry.Select().From("table").AndWhere("a = ?").SQL(),
+		"SELECT * FROM table WHERE b = ?":                    qry.Select().From("table").OrWhere("b = ?").SQL(),
+		"SELECT * FROM table WHERE a = ? AND b = ? OR c = ?": qry.Select().From("table").Where("a = ?").AndWhere("b = ?").OrWhere("c = ?").SQL(),
+		"SELECT a, b FROM table LIMIT 1 OFFSET 1":            qry.Select("a", "b").From("table").Limit(1).Offset(1).SQL(),
+		"SELECT * FROM table1 t1 JOIN table2 t2 ON t1.table2_id=t2.id LEFT JOIN table3 t3 ON t1.table3_id=t3.id RIGHT JOIN table4 t4 ON t1.table4_id=t4.id INNER JOIN table5 t5 ON t1.table5_id=t5.id": qry.Select().From("table1 t1").
 			Join("table2 t2", "t1.table2_id=t2.id").
 			LeftJoin("table3 t3", "t1.table3_id=t3.id").
 			RightJoin("table4 t4", "t1.table4_id=t4.id").
 			InnerJoin("table5 t5", "t1.table5_id=t5.id").
-			SQL()
+			SQL(),
+		"SELECT * FROM table ORDER BY a ASC, b DESC, c ASC": qry.Select().From("table").OrderBy("a").Desc("b").Asc("c").SQL(),
+		"WITH a AS 1 SELECT a":                              qry.Select("a").Prefix("WITH a AS 1").SQL(),
+		"SELECT 1 FOR UPDATE":                               qry.Select("1").Suffix("FOR UPDATE").SQL(),
+	}
 
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("order", func(t *testing.T) {
-		expected := "SELECT * FROM table ORDER BY a ASC, b DESC, c ASC"
-		result := qry.Select().From("table").OrderBy("a").Desc("b").Asc("c").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("prefix", func(t *testing.T) {
-		expected := "WITH a AS 1 SELECT a"
-		result := qry.Select("a").Prefix("WITH a AS 1").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
-
-	t.Run("suffix", func(t *testing.T) {
-		expected := "SELECT 1 FOR UPDATE"
-		result := qry.Select("1").Suffix("FOR UPDATE").SQL()
-
-		must := must.New(t)
-		must.Equal(expected, result)
-	})
+	for expected, result := range cases {
+		t.Run(expected, func(t *testing.T) {
+			must := must.New(t)
+			must.Equal(expected, result)
+		})
+	}
 
 }
